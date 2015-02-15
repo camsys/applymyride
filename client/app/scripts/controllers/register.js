ef6fac8839842326d1169ed1280bb2a502c8d309
Denis Haskin <denis@constantorbit.com>
2015-02-14 17:27:33 -0800

    Sign Up, Learn More paths (without text from Eric).

---
+++ client/app/scripts/controllers/register.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('applyMyRideApp')
+  .controller('RegisterController', ['$scope', '$location', 'flash',
+    function ($scope, $location, flash) {
+      $scope.detectLoginType = function() {
+        $scope.isEmail = /@/.test($scope.login.emailOrId);
+        $scope.isSharedRideId = !$scope.isEmail && /^\d+$/.test($scope.login.emailOrId);
+      }
+
+      $scope.submittable = function() {
+        var f = $scope.loginForm;
+        console.log(f.email);
+        console.log(f.email.$valid);
+        console.log(f.email.$viewValue);
+        console.log(f.sharedRideId.$viewValue);
+      }
+
+      $scope.register = function() {
+        flash.setMessage('Welcome, Eric!');
+        $location.path('/plan');
+      }
+    }
+  ]);
