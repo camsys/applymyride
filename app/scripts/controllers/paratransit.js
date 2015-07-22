ed4b8a96fd8a629fd692e6c9554701c3a1ca4fc2
dbenoff <dbenoff@camsys.com>
2015-07-21 18:11:24 -0700

    PAMF-290

--- app/scripts/controllers/paratransit.js
+++ app/scripts/controllers/paratransit.js
@@ -21,6 +21,7 @@ angular.module('applyMyRideApp')
           result.wait_startDesc = moment(result.wait_start).format('h:mm a');
           result.wait_endDesc = moment(result.wait_end).format('h:mm a');
           result.arrivalDesc = moment(result.arrival).format('h:mm a');
+          result.travelTime = humanizeDuration(result.negotiated_duration * 1000,  { units: ["hours", "minutes"], round: true });
           if(!result.booked  == true){
             $scope.booking_failed = true;
           }
