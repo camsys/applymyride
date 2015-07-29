b959f85aee19fa712ebb60964a1937dda06b4e2d
dbenoff <dbenoff@camsys.com>
2015-07-29 06:59:09 -0700

    update deploy script

--- deploy-demo.sh
+++ deploy-demo.sh
@@ -3,11 +3,9 @@ git checkout $1
 git pull
 bower install
 datetag=$1
-git tag -a $datetag -m 'Demo deploy'
 grunt build
 perl -pi.bak -e 's/\<head\>/\<head\> <base href="\/findmyride-demo\/" target="_blank" \/> /g' dist/index.html
 perl -pi.bak -e "s/deployed_version/$datetag /g" dist/index.html
 rm dist/*.bak
 s3cmd del s3://findmyride-demo/*
 s3cmd put dist/* s3://findmyride-demo -r -P
-git push --tags
