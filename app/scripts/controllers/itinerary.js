5313bcb32a57f6d654d88fbaeb07e5ab9be70c2f
dbenoff <dbenoff@camsys.com>
2015-07-29 06:53:11 -0700

    PAMF-193

--- app/scripts/controllers/itinerary.js
+++ app/scripts/controllers/itinerary.js
@@ -20,6 +20,28 @@ angular.module('applyMyRideApp')
           result.wait_endDesc = moment(result.wait_end).format('h:mm a');
           result.arrivalDesc = moment(result.arrival).format('h:mm a');
         });
+
+        if($scope.driverInstructions == null)
+          $scope.driverInstructions = 'N/A';
+
+        $scope.escort = "";
+        if($scope.trip.hasEscort == true){
+          $scope.escort += "1 Escort";
+        }
+
+        if($scope.trip.numberOfCompanions != null && $scope.trip.numberOfCompanions > 0){
+          if($scope.escort){
+            $scope.escort += ', ';
+          }
+          $scope.escort += $scope.trip.numberOfCompanions + ' Companion';
+          if(planService.numberOfCompanions > 1){
+            $scope.escort += 's';
+          }
+        }
+
+        if($scope.escort.length == 0)
+          $scope.escort = 'N/A';
+
       }
       $scope.mode = $scope.trip.mode;
 
--- deploy-demo.sh
+++ deploy-demo.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
-sed -i '' 's/\<head\>/\<head\> <base href="\/findmyride-demo\/" target="_blank" \/> /g' dist/index.html
+git checkout $
+git pull
+bower install
+datetag=$(date "+%Y%m%d%H%M").'_DEMO_deploy'
+git tag -a $datetag -m 'Demo deploy'
+grunt build
+perl -pi.bak -e 's/\<head\>/\<head\> <base href="\/findmyride-demo\/" target="_blank" \/> /g' dist/index.html
+perl -pi.bak -e "s/deployed_version/$datetag /g" dist/index.html
+rm dist/*.bak
 s3cmd del s3://findmyride-demo/*
 s3cmd put dist/* s3://findmyride-demo -r -P
+git push --tags
