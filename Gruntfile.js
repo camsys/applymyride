86669e0fbc38a27c22cbd190aeada2fce62b6a38
Josh Haglund <jhaglund@camsys.com>
2016-07-11 18:59:55 -0700

    removing uglify -- messing with angular framework

--- Gruntfile.js
+++ Gruntfile.js
@@ -305,8 +305,8 @@ module.exports = function (grunt) {
         flow: {
           html: {
             steps: {
-              js: ['concat', 'uglifyjs'],
-              //js: ['concat'],
+              //js: ['concat', 'uglifyjs'],
+              js: ['concat'],
               css: ['cssmin']
             },
             post: {}
