74a6b3b523e2fd82b917e32eb776d7158517df95
lcaraballo <lcaraballo@gmail.com>
2016-07-18 09:07:47 -0400

    PAMF-393 - New Trip link now goes to new /plan/where page

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -22,7 +22,7 @@ angular.module('applyMyRideApp')
 
       $scope.reset = function() {
         planService.reset();
-        $location.path("/plan/fromDate");
+        $location.path("/plan/where");
       };
 
       $scope.showNavbar = function() {
