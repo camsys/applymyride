cc996a754f5f230ffa8a32b55ef5295091c936b7
W. Scott Meeks <smeeks@camsys.com>
2021-02-19 14:26:20 -0500

    Updated deploy scripts to reference new dev & qa instances

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-aws s3 sync ./dist/ s3://findmyridepa2-dev.camsys-apps.com/ --acl public-read
+aws s3 sync ./dist/ s3://ui-pa-dev/ --acl public-read
 
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation \
-    --distribution-id E9FFXEIL8WAPY \
+    --distribution-id E1TB3A9G4UWN1L \
     --paths '/*'
 
