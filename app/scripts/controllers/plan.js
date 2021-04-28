bd0bfb0de536a5a54569f471c36a64c5f9fc7e1e
Gerard Vaio <VAIOG-MAC@VAIOG-MAC.home>
2021-04-28 14:12:53 -0400

    [PAMF-690] fix

--- app/scripts/controllers/plan.js
+++ app/scripts/controllers/plan.js
@@ -1651,9 +1651,9 @@ function($scope, $http, $routeParams, $location, planService, util, flash, usSpi
       $scope.hasTransit = $scope.transitInfos.length > 0;
       $scope.hasWalk = $scope.walkItineraries.length > 0;
 
-      const firstItinerary = $scope.transitItineraries[0][0]
-      const firstTransit = firstItinerary.json_legs.find(leg => leg.mode === 'BUS')
-      $scope.transitRoute = firstTransit ? firstTransit.route : undefined
+      const firstItinerary = $scope.transitItineraries && $scope.transitItineraries[0] && $scope.transitItineraries[0][0];
+      const firstTransit = firstItinerary && firstItinerary.json_legs.find(leg => leg.mode === 'BUS');
+      $scope.transitRoute = firstTransit ? firstTransit.route : undefined;
 
       if($scope.paratransitItineraries.length < 1 && $scope.transitItineraries.length < 1 && $scope.walkItineraries.length < 1 && !$scope.hasUber && !$scope.hasTaxi){
         $scope.noresults = true;
